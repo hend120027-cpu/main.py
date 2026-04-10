900a1a1e687bbe4807e95dd2576ade805431fe58
hend120027-cpu <hend120027@gmail.com>
2026-04-11 01:53:38 +0200

    main.py

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ from telegram.ext import (
     Application, CommandHandler, MessageHandler, filters, ContextTypes
 )
 
-TOKEN = '7834120140:AAFKg-uBhB6ZFpDQqf3imLqyX9X2E2qO_XE'
+TOKEN = '8647240736:AAEGXuwmtZkUvAfbURX2BcyyuoWD-TekP_0'
 
 # ------------------- Users -------------------
 
