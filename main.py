b5d63dd8878cb5403f9852e54725de14798ff2b6
hend120027-cpu <hend120027@gmail.com>
2026-04-13 13:36:45 +0200

    main.py

--- main.py
+++ main.py
@@ -28,8 +28,7 @@ user_tasks = {}
 
 GATES = [
     "https://raybensch.com/donations/support-ray/",
-    "https://www.mgn1.org/events/",
-    "https://9millionreasons.nyc/donations/4293-2/"
+    "https://www.mgn1.org/events/"
 ]
 gate_index = 0
 api_semaphore = asyncio.Semaphore(6)
